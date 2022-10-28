0abce432cd2c16adfc79c1395590ab43d7df0b04
IchBinBekifft <73794066+Clegames@users.noreply.github.com>
2022-10-28 10:05:35 +0200

    Added bugfix in 'build.gradle.kts'

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -20,6 +20,8 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
     compileOnly("io.papermc.paper:paper-api:1.19.2-R0.1-SNAPSHOT")
+    implementation("org.lombok:lombok:1.18.22")
+    annotationProcessor("org.projectlombok:lombok:1.18.22")
 }
 
 tasks.getByName<Test>("test") {
